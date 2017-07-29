5178c6979316462fc1e5d5f236cade3575edb1ef
Ivan Esparza <ivanebernal@gmail.com>
2017-07-29 02:40:11 -0700

    Added phone intent and Dolores picture

--- app/src/main/java/com/ivanebernal/summertalksapp/MainActivity.kt
+++ app/src/main/java/com/ivanebernal/summertalksapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ivanebernal.summertalksapp
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -18,14 +20,17 @@ class MainActivity : AppCompatActivity(), ContactsAdapter.ContactsListener {
     }
 
     override fun onContactSelected(contact: Contact) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val phoneNumber = contact.phoneNumber
+        val phoneIntent = Intent(Intent.ACTION_DIAL)
+        phoneIntent.data = Uri.parse( "tel:$phoneNumber")
+        startActivity(phoneIntent)
     }
 
     fun getContacts(): List<Contact>{
-        val ivan = Contact("Ivan", "6622222222", R.drawable.ic_launcher_background, "Narnia 123")
-        val cassandra = Contact("Cassandra", "6621111111", R.drawable.ic_launcher_background, "Hyrule 345")
-        val fulanito = Contact("Cosme Fulanito", "6621212121", R.drawable.ic_launcher_background, "Mision Juriquilla 242")
-        val menganito = Contact("Mengano", "6622212121", R.drawable.ic_launcher_background, "Macondo 100")
+        val ivan = Contact("Ivan", "6622222222", R.drawable.dolores, "Narnia 123")
+        val cassandra = Contact("Cassandra", "6621111111", R.drawable.dolores, "Hyrule 345")
+        val fulanito = Contact("Cosme Fulanito", "6621212121", R.drawable.dolores, "Mision Juriquilla 242")
+        val menganito = Contact("Mengano", "6622212121", R.drawable.dolores, "Macondo 100")
         return listOf(ivan, cassandra, fulanito, menganito)
     }
 }
